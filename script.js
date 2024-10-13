cad4d0a08dcb15f651e4b078b7fe7316e7d737ea
John Caleb <Calebjohn3112@gmail.com>
2024-10-13 22:03:37 +0100

    Made js shorter with for loop

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ let dollarIcon = document.querySelector(".d-sign");
 let userIcon = document.querySelector(".u-sign");
 let resetButton = document.querySelector(".reset");
 
-
 noOfPeople.addEventListener("keyup", function(){
     totalBill.value / noOfPeople.value
 });
 
 for (let index = 0; index < tipPercentage.length; index++) {
     tipPercentage[index].addEventListener("click", function(){
-        let tipPerPerson = index === 5 ? (20 / 100) :valueOfTip[index].textContent * (totalBill.value / noOfPeople.value);        
+        if(totalBill.value == 0) return;
+        if(noOfPeople.value == 0) return;
+        let tipPerPerson = index === 5 ? (20 / 100) * (totalBill.value / noOfPeople.value) : (valueOfTip[index].textContent / 100) * (totalBill.value / noOfPeople.value);
         let totalTip = tipAmountPerPerson.textContent = tipPerPerson
         totalBillPerPerson.textContent = totalTip + (totalBill.value / noOfPeople.value)
         dollarIcon.style.display = "none"
         userIcon.style.display = "none"
-    });   
+    });
 }
 
 resetButton.addEventListener("click", function(){
     totalBillPerPerson.textContent = "0.00";
     tipAmountPerPerson.textContent = "0.00";
-    noOfPeople.value = 0;
-    totalBill.value = 0;
+    noOfPeople.value = "";
+    totalBill.value = "";
 });
